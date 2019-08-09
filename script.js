ce9a20762b5b3d50181dcf47e29c727fffc4182d
ruaridhm <ruaridh.mcguckin@gmail.com>
2019-08-09 16:24:33 +0100

    Add files via upload
    added category selection functionality before selecting I dont get it button

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ function myFunction() {
 
 //array of links for each catagory
 
-var catagory = [
+var comments = [
     "https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code",
     "https://www.w3schools.com/js/js_comments.asp",
     "https://javascript.info/comments",
     "https://www.w3resource.com/javascript/introduction/javascript-syntax-comments.php"
 ]; 
 
+var variables = [
+  "https://www.w3schools.com/js/js_variables.asp"
+]
 
+var catagory = "";
 
 function cycle(){
 
@@ -31,3 +35,19 @@ function cycle(){
 
     //ignore line underneath modify and use it if needed in cycle function.
     //document.getElementById("dont").innerHTML
+
+
+
+
+
+    //code for currently selected catagory
+
+function assignCatagory(x){
+  console.log(catagory);
+  catagory = [x];
+  console.log(catagory);
+}
+
+    
+
+  
